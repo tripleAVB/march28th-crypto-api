68144378988d20bcb3f79418151ee7b3dc48ae85
Demetrius Ferreira <demithehomie@gmail.com>
2024-04-10 08:33:22 -0300

    Profitability

---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+ * 1 - MONTORAMENTO
+ * 
+ * 2- ESTRATÉGIA
+ * 
+ * 3 - TRADES
+ * 
+ * 
+ */
+const http = require('http');
+//const dotenv = require('dotenv');
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, world!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+const WebSocket = require("ws");
+const ws = new WebSocket(`${process.env.STREAM_URL_DEV}/${process.env.SYMBOL.toLowerCase()}@ticker`);
+
+const PROFITABILITY = parseFloat(process.env.PROFITABILITY);
+let sellPrice = 0;
+
+ws.onmessage = (event) => {
+    console.clear();
+    const obj = JSON.parse(event.data);
+    console.log(`Symbol: ${obj.s}`);
+    console.log(`Ask Price: ${obj.a}`);
+
+    const currentPrice = parseFloat(obj.a);
+    if (sellPrice == 0 && currentPrice < 16000 ) {
+        console.log('Comprar');
+        sellPrice = currentPrice + (currentPrice * PROFITABILITY);
+        //
+
+    } else if (currentPrice > 16000) {
+        console.log('Vender');
+    }
+    console.log(`Esperando..`);
+    //console.log(`Price: ${currentPrice}`);
+
+}
